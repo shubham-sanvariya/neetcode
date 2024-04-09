e08becc07d76368b2d73c68f46673f754b6fc951
Shubham Sanwariya <shubhamsanvariyait@gmail.com>
2024-04-09 12:02:54 +0530

    28th day Reverse_Linked_List

---
+++ Reverse_Linked_List.java
@@ -0,0 +1,30 @@
+public class Reverse_Linked_List {
+    public static void main(String[] args) {
+        ListNode listNode = new ListNode(1);
+        listNode.next = new ListNode(2);
+        listNode.next.next = new ListNode(3);
+        Reverse_Linked_List reverseLinkedList = new Reverse_Linked_List();
+        reverseLinkedList.reverseList(listNode);
+    }
+
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+
+        return prev;
+    }
+
+    public static class ListNode {
+     int val;
+     ListNode next;
+     ListNode() {}
+     ListNode(int val) { this.val = val; }
+     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
+}
