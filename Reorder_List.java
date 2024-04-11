56366f990589500e15210daae1778c44ec247e34
Shubham Sanwariya <shubhamsanvariyait@gmail.com>
2024-04-11 11:43:01 +0530

    30th day Reorder_List

---
+++ Reorder_List.java
@@ -0,0 +1,56 @@
+public class Reorder_List {
+    public static void main(String[] args) {
+        ListNode listNode = new ListNode(1);
+        listNode.next = new ListNode(2);
+//        listNode.next.next = new ListNode(3);
+//        listNode.next.next.next = new ListNode(4);
+//        listNode.next.next.next.next = new ListNode(5);
+        Reorder_List reorderList = new Reorder_List();
+        reorderList.reorderList(listNode);
+    }
+
+    public void reorderList(ListNode head) {
+        if (head == null) return;
+
+        // find the middle of linked list [Problem 876]
+        // in 1->2->3->4->5->6 find 4
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        // reverse the second part of the list [Problem 206]
+        // convert 1->2->3->4->5->6 into 1->2->3->4 and 6->5->4
+        // reverse the second half in-place
+        ListNode prev = null, curr = slow, tmp;
+        while (curr != null) {
+            tmp = curr.next;
+
+            curr.next = prev;
+            prev = curr;
+            curr = tmp;
+        }
+
+        // merge two sorted linked lists [Problem 21]
+        // merge 1->2->3->4 and 6->5->4 into 1->6->2->5->3->4
+        ListNode first = head, second = prev;
+        while (second.next != null) {
+            tmp = first.next;
+            first.next = second;
+            first = tmp;
+
+            tmp = second.next;
+            second.next = first;
+            second = tmp;
+        }
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+}
