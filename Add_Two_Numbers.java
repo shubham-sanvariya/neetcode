da1399f839faee977ffbd5cd6e1e270fa85f7c8e
Shubham Sanwariya <shubhamsanvariyait@gmail.com>
2024-04-14 12:27:21 +0530

    33rd day Copy_List_With_Random_Pointer

---
+++ Add_Two_Numbers.java
@@ -0,0 +1,43 @@
+public class Add_Two_Numbers {
+    public static void main(String[] args) {
+        Add_Two_Numbers addTwoNumbers = new Add_Two_Numbers();
+        ListNode listNode = new ListNode(2);
+        listNode.next = new ListNode(4);
+        listNode.next.next = new ListNode(3);
+
+        ListNode listNode2 = new ListNode(5);
+        listNode2.next = new ListNode(6);
+        listNode2.next.next = new ListNode(4);
+        addTwoNumbers.addTwoNumbers(listNode,listNode2);
+    }
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+        int carry = 0;
+
+        while (l1 != null || l2 != null || carry > 0) {
+            if (l1 != null) {
+                carry += l1.val;
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                carry += l2.val;
+                l2 = l2.next;
+            }
+            curr.next = new ListNode(carry % 10);
+            carry /= 10;
+            curr = curr.next;
+        }
+
+        return dummy.next;
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val,ListNode next) { this.val = val; this.next = next; }
+    }
+}
