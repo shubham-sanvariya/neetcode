0247e83d4e86a2accc48374fff8cc90547eb8f2e
Shubham Sanwariya <shubhamsanvariyait@gmail.com>
2024-04-30 17:33:08 +0530

    49th day Balanced_Binary_Tree

--- Balanced_Binary_Tree.java
+++ Balanced_Binary_Tree.java
@@ -11,17 +11,17 @@ public class Balanced_Binary_Tree {
     }
 
     public boolean isBalanced(TreeNode root) {
-        if (root == null)
-            return true;
-        return Math.abs(maxDepth(root.left) - maxDepth(root.right)) <= 1 && //
-                isBalanced(root.left) &&                                        //
-                isBalanced(root.right);
+        return getDepth(root) != -1;
     }
 
-    private int maxDepth(TreeNode root) {
-        if (root == null)
-            return 0;
-        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+    private int getDepth(TreeNode root) {
+        if (root == null) return 0;
+        int left = getDepth(root.left);
+        if (left == -1) return -1;
+        int right = getDepth(root.right);
+        if (right == -1) return -1;
+        if (Math.abs(left - right) > 1) return -1;
+        return 1 + Math.max(left, right);
     }
 
     static class TreeNode {
