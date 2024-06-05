f9be9a1cd8606787f91d893aab9fb9b60be7a202
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-06-05 10:27:05 +0530

    85 day Subsets

---
+++ Subsets.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Subsets {
+    public static void main(String[] args) {
+        int[] nums = {1,2,3};
+        Subsets subsets = new Subsets();
+        subsets.subsets(nums);
+    }
+
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        dfs(nums, 0, new ArrayList<>(), ans);
+        return ans;
+    }
+
+    private void dfs(int[] nums, int s, List<Integer> path, List<List<Integer>> ans) {
+        ans.add(new ArrayList<>(path));
+
+        for (int i = s; i < nums.length; ++i) {
+            path.add(nums[i]);
+            dfs(nums, i + 1, path, ans);
+            path.remove(path.size() - 1);
+        }
+    }
+}
